68e9d57d08b30841b867e6a305ff6e8de2cd962d
ilkhomk <i.kamolov1@gmail.com>
2020-08-21 07:30:25 +0000

    new ec2

--- ec2.tf
+++ ec2.tf
@@ -4,5 +4,13 @@ resource "aws_instance" "web" {
   key_name  = "${aws_key_pair.class.key_name}" 
   vpc_security_group_ids = ["${aws_security_group.vpc_task.id}"]
   subnet_id = "${aws_subnet.public1.id}"
-   associate_public_ip_address = true
+  associate_public_ip_address = "true"
+}
+resource "aws_instance" "mysql" {
+  ami           = "${data.aws_ami.centos.id}" 
+  instance_type = "t2.micro"
+  key_name  = "${aws_key_pair.class.key_name}" 
+  vpc_security_group_ids = ["${aws_security_group.sql_task.id}"]
+  subnet_id = "${aws_subnet.public2.id}"
+  associate_public_ip_address = "true"
 }
--- sec_group.tf
+++ sec_group.tf
@@ -26,3 +26,38 @@ resource "aws_security_group" "vpc_task" {
   tags          = "${var.tags}"
 
 }
+resource "aws_security_group" "sql_task" {
+  name        = "sql_task"
+  description = "Allow mysql inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "Allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow mysql"
+    from_port   = 3306 
+    to_port     = 3306 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags          = "${var.tags}"
+
+}
